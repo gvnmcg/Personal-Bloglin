71c229a3d68bcbc218507d5f1ad2d73b14688b8e
gvnmcg <gvnmcg517@gmail.com>
2022-12-19 18:09:50 -0700

    replace markdown-it with react-marked-markdown

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import matter from 'gray-matter';
-import md from 'markdown-it';
+import { MarkdownPreview } from 'react-marked-markdown';
 
 export async function getStaticPaths() {
   const files = fs.readdirSync('posts');
@@ -30,7 +30,7 @@ export default function PostPage({ frontmatter, content }) {
   return (
     <div className='prose mx-auto'>
       <h1>{frontmatter.title}</h1>
-      {/* <div dangerouslySetInnerHTML={{ __html: md().render(content) }} /> */}
+      <MarkdownPreview value={ content }/>
     </div>
   );
 }
